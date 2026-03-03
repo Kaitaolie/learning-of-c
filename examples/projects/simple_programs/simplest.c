5c35c027509ada1f10371d897468162378587159
Kaitaolie <kaitaolie@gmail.com>
2026-03-03 19:06:15 +0800

    siplest program

---
+++ examples/projects/simple_programs/simplest.c
@@ -0,0 +1,19 @@
+//This is a simplest C program, with nothing output.
+//However, it's the skeletal struture upon which almost every C application is built. 
+//It tells the compiler to include the contents of the Standard Input Output header file(stdio.h).
+//int: This is the return type. It tells the operating system thay when this program finishes, it will send back an integer value(usually 0 for success).
+//main: This is the "front door" of your program. Every C program  must have a function named main. When you run the code, the executinon starts exactly here.
+// (): These parentheses are for parameters. In this simple version, they are empty, meaning the function doesn't require any extra information to start.
+//The Exit Code: This line ends the main function.
+//The Logic: In C, returning 0 is a universal signal to the Operating System that the program executed successfully without any errors. If something had gone wrong, you might return a non-zero number (like 1).
+//";": In C, the ; acts like a period at the end of a sentence. It tells the compiler that the instruction is complete.
+//The "{}" define the scope of the function.
+//Everything written inside these braces is the actual "work" the program performs. Think of it as the container for your logic.
+
+#include <stdio.h>     
+int main()
+{
+    
+    return 0;
+    
+}
